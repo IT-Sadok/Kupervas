e908a98e6ea3e55b3cd808c60ed98cc219c1852c
Ruslan <rkupervas2003@gmail.com>
2024-05-24 00:10:57 +0300

    Update Program.cs

--- Dom.zavd 1/Program.cs
+++ Dom.zavd 1/Program.cs
@@ -1,32 +1,29 @@
 using System;
 
 Salon cars = new Salon();
-    Console.WriteLine("Choose the option");
-    Console.WriteLine("1.Add car");
-    Console.WriteLine("2.Remove car");
+Console.WriteLine("Choose the option");
+Console.WriteLine("1.Add car");
+Console.WriteLine("2.Remove car");
 string Choice = Console.ReadLine();
 switch (Choice) 
-    {
-        case "1":
-        AddCar(cars);
-            break;
-        case "2":
-        RemoveCar(cars);
-            break;
-    }
-     static void AddCar(Salon Car)
 {
-    Console.WriteLine("Введіть марку авто");
-    string Brand = Console.ReadLine();
-    Console.WriteLine("Введіть модель авто");
-    string Model = Console.ReadLine();
-    Console.WriteLine("Введіть рік виготовлення авто");
-    int.TryParse(Console.ReadLine(), out int Year);
+    case "1":
+       AddCar(cars);
+    break;
+    case "2":
+        RemoveCar(cars);
+    break;
 }
-     static void RemoveCar(Salon cars)
-      {
-        cars.RemoveCar();
-      }
+static void AddCar(Salon cars)
+{
+  Console.WriteLine("Введіть марку авто");
+  string Brand = Console.ReadLine();
+  cars.AddCar(Brand);
+}
+static void RemoveCar(Salon cars)
+{
+ cars.RemoveCar();
+}
   
 
 
