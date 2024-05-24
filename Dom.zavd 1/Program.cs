0819bef3c9712b5775f361bf222ef1a3232dcd85
Ruslan <rkupervas2003@gmail.com>
2024-05-24 23:38:58 +0300

    Update Program.cs

--- Dom.zavd 1/Program.cs
+++ Dom.zavd 1/Program.cs
@@ -1,29 +1,32 @@
 using System;
+salon cars = new salon();
+while (true)
+{
+    Console.WriteLine("Choose the option");
+    Console.WriteLine("1.Add car");
+    Console.WriteLine("2.Remove car");
+    string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            AddCar(cars);
+            break;
+        case "2":
+            RemoveCar(cars);
+            break;
+    }
+    static void AddCar(salon cars)
+    {
+        Console.WriteLine("Введіть марку авто");
+        string Brand = Console.ReadLine();
+        cars.AddCar(Brand);
+    }
+    static void RemoveCar(salon cars)
+    {
+        cars.RemoveCar();
+    }
+}
 
-Salon cars = new Salon();
-Console.WriteLine("Choose the option");
-Console.WriteLine("1.Add car");
-Console.WriteLine("2.Remove car");
-string choice = Console.ReadLine();
-switch (choice) 
-{
-    case "1":
-       AddCar(cars);
-    break;
-    case "2":
-        RemoveCar(cars);
-    break;
-}
-static void AddCar(Salon cars)
-{
-  Console.WriteLine("Введіть марку авто");
-  string Brand = Console.ReadLine();
-  cars.AddCar(Brand);
-}
-static void RemoveCar(Salon cars)
-{
- cars.RemoveCar();
-}
-  
+
 
 
