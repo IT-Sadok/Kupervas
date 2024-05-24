a026d8851d095c1f2abbad6da877613e4458952a
Ruslan <rkupervas2003@gmail.com>
2024-05-24 22:43:47 +0300

    Add files via upload

---
+++ Dom.zavd 1/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Car
+    {
+        private string Brand { get; set; }
+        public Car(string Brand)
+        {
+            Brand = Brand;
+        }
+        public override string ToString()
+        {
+            return Brand;
+        }
+    }
+}
