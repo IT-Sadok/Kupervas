ef1e247a93b67853d3775a54950dc3b23867b56a
Ruslan <rkupervas2003@gmail.com>
2024-05-24 23:02:24 +0300

    Update Salon.cs

--- Dom.zavd 1/Salon.cs
+++ Dom.zavd 1/Salon.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-public class salon
+public class Salon
 {
     private int carCount;
-    public salon()
+    public Salon()
     {
         carCount = 0;
     }
