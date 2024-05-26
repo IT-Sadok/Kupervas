85bdf5ca7617b47d081368c7f831d8e634f12eac
Ruslan <rkupervas2003@gmail.com>
2024-05-26 09:43:52 +0300

    Update Salon.cs

--- Dom.zavd 1/Salon.cs
+++ Dom.zavd 1/Salon.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 public class Salon
 {
-    private int carCount;
-    public Salon()
-    {
-        carCount = 0;
-    }
+    public int carCount = 0;
     public void AddCar(string Brand)
     {
         carCount++;
-        Console.WriteLine("Car added successfully: " + carCount );
-
     }
     public void RemoveCar()
     {
         if ( carCount > 0 )
         {
             carCount--;
-            Console.WriteLine("Car removed successfully: " + carCount);
+            Console.WriteLine("Car removed successfully: " +carCout ) // But is it good here according to S principle?
         }
         else
         {
