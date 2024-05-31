67a0353968b2fc2ad81d4e225690af6ebb8853b1
Ruslan <rkupervas2003@gmail.com>
2024-05-31 22:43:48 +0300

    Update Salon.cs

--- Dom.zavd 1/Salon.cs
+++ Dom.zavd 1/Salon.cs
@@ -1,3 +1,5 @@
+
+using ConsoleApp2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,13 +8,26 @@ using System.Threading.Tasks;
 
 public class Salon
 {
-    public int carCount{ get; private set; } 
-    public void AddCar(string Brand)
+    private List<Car> cars = new List<Car>();
+    public int carCount { get; private set; }
+    public void AddCar(string Brand, string Model, int Year)
     {
+        cars.Add(new Car(Brand, Model, Year));
         carCount++;
     }
     public void RemoveCar()
     {
         carCount--;
+    }
+    public void ShowCars()
+    {
+        foreach (Car car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+    public List<Car> GetAllCars() 
+    {
+        return new List<Car>(cars);
     }
 }
