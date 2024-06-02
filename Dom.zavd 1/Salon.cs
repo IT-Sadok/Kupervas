d62d01ae68759a940118ab6109f3bb8fbe8f9485
Ruslan <rkupervas2003@gmail.com>
2024-06-02 23:38:33 +0300

    Update Salon.cs

--- Dom.zavd 1/Salon.cs
+++ Dom.zavd 1/Salon.cs
@@ -8,23 +8,17 @@ using System.Threading.Tasks;
 
 public class Salon
 {
-    private List<Car> cars = new List<Car>();
-    public int carCount { get; private set; }
+    public List<Car> cars = new List<Car>();
+    public int CarCount { get; private set; }
     public void AddCar(string Brand, string Model, int Year)
     {
-        cars.Add(new Car(Brand, Model, Year));
-        carCount++;
+        cars.Add(item: new Car(Brand, Model, Year));
+        CarCount++;
     }
     public void RemoveCar()
     {
-        carCount--;
-    }
-    public void ShowCars()
-    {
-        foreach (Car car in cars)
-        {
-            Console.WriteLine(car);
-        }
+        cars.RemoveAt(cars.Count - 1);
+        CarCount--;
     }
     public List<Car> GetAllCars() 
     {
