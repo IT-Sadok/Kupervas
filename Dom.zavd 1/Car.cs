9b03daecf695029d126aab80dc55740b3c87cbd9
Ruslan <rkupervas2003@gmail.com>
2024-06-16 22:49:17 +0300

    Update Car.cs

--- Dom.zavd 1/Car.cs
+++ Dom.zavd 1/Car.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,18 +7,5 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    public class Car
-    {
-        public Car(string brand, string model, int year)
-        {
-            Brand = brand;
-            Model = model;
-            Year = year;
-        }
-
-        private string Brand { get; set; }
-        private string Model { get; set; }
-        private int Year { get; set; }
-        public record Car(string Brand, string Model, int Year);
-    }
+    public record Car (string Brand, string Model, int Year);
 }
